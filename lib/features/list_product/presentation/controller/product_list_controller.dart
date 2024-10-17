f63f174f24ef8c4504746ad1b0dc14a09324d293
B2014776 <leminhphuong899@gmail.com>
2024-10-17 20:42:37 +0700

    chinh lai

--- lib/features/list_product/presentation/controller/product_list_controller.dart
+++ lib/features/list_product/presentation/controller/product_list_controller.dart
@@ -19,7 +19,7 @@ class ProductListController extends GetxController {
       var response = await http.get(
         Uri.parse('https://api-luan-v1.onrender.com/api/v1/products/'),
         headers: {
-          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI5MTA1MjY0LCJpYXQiOjE3MjkwOTQ0NjQsImp0aSI6ImQzN2IwMDRjY2UzMTRmN2E5NTk1NGZkZGQxMjFlZTE2IiwidXNlcl9pZCI6IjE2OTVkYWU1LWUzNTgtNDUwYy1hZjYyLTFlNTYzY2ZhZGM0NyIsInVzZXJuYW1lIjoiYWRtaW4iLCJlbWFpbCI6InBodW9uZ2N1dGVAZ21haWwuY29tIn0.wyqy7ePGsXec7GLfhE5Hk1LC8iFI5fhOW6l7oktxnDk',
+
           'Content-Type': 'application/json',
         },
       );
